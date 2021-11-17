f51db859d37be7eb664ee215ef2382cfa34c3659
CGonzalez97 <cgl76490@gmail.com>
2021-11-17 12:11:10 +0100

    Ya funciona

--- src/main/java/com/ntt_data_bootcamp/microservicios/MicroserviciosSpringMetricasRetoClientApplication.java
+++ src/main/java/com/ntt_data_bootcamp/microservicios/MicroserviciosSpringMetricasRetoClientApplication.java
@@ -1,5 +1,7 @@
 package com.ntt_data_bootcamp.microservicios;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,6 +15,8 @@ public class MicroserviciosSpringMetricasRetoClientApplication implements Comman
 
 	@Autowired
 	MetricsService servicio;
+	
+	Logger logger = LoggerFactory.getLogger(MicroserviciosSpringMetricasRetoClientApplication.class);
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviciosSpringMetricasRetoClientApplication.class, args);
@@ -29,14 +33,17 @@ public class MicroserviciosSpringMetricasRetoClientApplication implements Comman
 		for(int i = 0; i < 5; i++) {
 			sys.setStatus(halfOpen);
 			servicio.setToHalfOpen();
+			logger.info(halfOpen);
 			Thread.sleep(2000);
 			if(i <= 3) {
 				sys.setStatus(open);
 				servicio.setToOpen();
+				logger.info(open);
 				Thread.sleep(2000);
 			}else {
 				sys.setStatus(close);
 				servicio.setToClose();
+				logger.info(close);
 				Thread.sleep(2000);
 			}
 		}
--- src/main/java/com/ntt_data_bootcamp/microservicios/services/MetricsService.java
+++ src/main/java/com/ntt_data_bootcamp/microservicios/services/MetricsService.java
@@ -2,33 +2,41 @@ package com.ntt_data_bootcamp.microservicios.services;
 
 import java.net.URI;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.ntt_data_bootcamp.microservicios.model.Sistema;
+
 @Service
 public class MetricsService {
-	
+		
 	RestTemplate restTemplate = new RestTemplate();
 	String urlBase = "http://localhost:8080/actuator/metricas";
 	String openEndPoint = "open";
 	String closeEndPoint = "close";
 	String halfOpen = "half-open";
 	URI url = URI.create(urlBase);
+	Sistema sys = new Sistema();
 	
 	public void setToOpen() {
+		sys.setStatus(openEndPoint);
 		ResponseEntity<String> response
-		  = restTemplate.postForEntity(URI.create(urlBase+"/"+openEndPoint), openEndPoint, String.class);
+		  = restTemplate.postForEntity(URI.create(urlBase+"/"+openEndPoint), sys, String.class);
 	}
 	
 	public void setToClose() {
+		sys.setStatus(openEndPoint);
 		ResponseEntity<String> response
-		  = restTemplate.postForEntity(URI.create(urlBase+"/"+closeEndPoint), closeEndPoint, String.class);
+		  = restTemplate.postForEntity(URI.create(urlBase+"/"+closeEndPoint), sys, String.class);
 	}
 	
 	public void setToHalfOpen() {
+		sys.setStatus(openEndPoint);
 		ResponseEntity<String> response
-		  = restTemplate.postForEntity(URI.create(urlBase+"/"+halfOpen), halfOpen, String.class);
+		  = restTemplate.postForEntity(URI.create(urlBase+"/"+halfOpen), sys, String.class);
 	}
 	
 	public String getStatus() {
